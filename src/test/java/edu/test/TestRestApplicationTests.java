43f0dc57369078dfb235ee38893725af41f6ac73
ike3 <ike3@server.org>
2020-11-10 15:05:06 +0300

    minimal code

---
+++ src/test/java/edu/test/TestRestApplicationTests.java
@@ -0,0 +1,15 @@
+package edu.test;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class TestRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
